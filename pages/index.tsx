35ddb846e1a3afe3e6baeb484cccfafaa3068f7f
Cody Clark <opuseleven@protonmail.com>
2022-02-18 14:53:17 -0600

    Added handleClick for all number buttons.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import styles from '../styles/Home.module.css';
 import { useState } from 'react';
 import { ViewScreen, ClearButton, NegativeButton, DivideButton, NumberButton } from '../components';
 import { MultiplyButton, SubtractButton, AddButton, ZeroButton, DecimalButton, EqualsButton } from '../components';
+import { handleNumberClick } from '../services';
 
 const Home: NextPage = () => {
 
@@ -32,17 +33,17 @@ const Home: NextPage = () => {
             <ClearButton />
             <NegativeButton />
             <DivideButton />
-            <NumberButton number={7} />
-            <NumberButton number={8} />
-            <NumberButton number={9} />
+            <NumberButton number={7} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 7))} />
+            <NumberButton number={8} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 8))} />
+            <NumberButton number={9} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 9))} />
             <MultiplyButton />
-            <NumberButton number={4} />
-            <NumberButton number={5} />
-            <NumberButton number={6} />
+            <NumberButton number={4} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 4))} />
+            <NumberButton number={5} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 5))} />
+            <NumberButton number={6} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 6))} />
             <SubtractButton />
-            <NumberButton number={1} />
-            <NumberButton number={2} />
-            <NumberButton number={3} />
+            <NumberButton number={1} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 1))} />
+            <NumberButton number={2} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 2))} />
+            <NumberButton number={3} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 3))} />
             <AddButton />
             <ZeroButton />
             <DecimalButton />
