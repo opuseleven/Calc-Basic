273a2e9e9699551c1eb400238df8c06394bb7b8b
Cody Clark <opuseleven@protonmail.com>
2022-02-18 14:51:18 -0600

    Fixed handleClick

--- components/NumberButton.jsx
+++ components/NumberButton.jsx
@@ -1,12 +1,12 @@
 import styles from '../styles/Components.module.css';
 
-function NumberButton({ number }) {
+function NumberButton({ number, handleClick }) {
 
   const numberstring = String(number);
 
   return (
     <div className={styles[`button${numberstring}`]}>
-      <button className={styles.button}>{number}</button>
+      <button className={styles.button} onClick={handleClick}>{number}</button>
     </div>
   )
 }
