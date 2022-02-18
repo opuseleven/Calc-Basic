23f82f72ada19d1379b4e184ae06c994840a9a22
Cody Clark <opuseleven@protonmail.com>
2022-02-18 11:13:02 -0600

    Outlined UI with components.

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,15 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { useState } from 'react';
+import { ViewScreen, ClearButton, NegativeButton, DivideButton, NumberButton } from '../components';
+import { MultiplyButton, SubtractButton, AddButton, ZeroButton, DecimalButton, EqualsButton } from '../components';
 
 export default function Home() {
+
+  const [viewedNumber, setViewedNumber] = useState(0);
+  const [storedNumber, setStoredNumber] = useState(0);
+  const [sign, setSign] = useState("");
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,10 +22,36 @@ export default function Home() {
         <h1 className={styles.title}>
           Calc-Basic
         </h1>
+
+        <div className={styles.calculatorcontainer}>
+
+          <ViewScreen number={viewedNumber}/>
+
+          <div className={styles.buttonscontainer}>
+            <ClearButton />
+            <NegativeButton />
+            <DivideButton />
+            <NumberButton number={7} />
+            <NumberButton number={8} />
+            <NumberButton number={9} />
+            <MultiplyButton />
+            <NumberButton number={4} />
+            <NumberButton number={5} />
+            <NumberButton number={6} />
+            <SubtractButton />
+            <NumberButton number={1} />
+            <NumberButton number={2} />
+            <NumberButton number={3} />
+            <AddButton />
+            <ZeroButton />
+            <DecimalButton />
+            <EqualsButton />
+          </div>
+        </div>
       </main>
 
       <footer className={styles.footer}>
-        
+
       </footer>
     </div>
   )
