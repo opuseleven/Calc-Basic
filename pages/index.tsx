7709c68d0e2ec0aa5d144e6265bd8a1a29843ee1
Cody Clark <opuseleven@protonmail.com>
2022-02-18 20:04:44 -0600

    Added decimal boolean and implemented functionality into decimal button.

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ import styles from '../styles/Home.module.css';
 import { useState } from 'react';
 import { ViewScreen, ClearButton, NegativeButton, DivideButton, NumberButton } from '../components';
 import { MultiplyButton, SubtractButton, AddButton, ZeroButton, DecimalButton, EqualsButton } from '../components';
-import { handleNumberClick, handleClearClick, handleSignClick, handleEqualsClick, handleNegativeClick } from '../services';
+import { handleNumberClick, handleClearClick, handleSignClick, handleEqualsClick, handleNegativeClick, handleDecimalClick } from '../services';
 
 const Home: NextPage = () => {
 
@@ -12,6 +12,7 @@ const Home: NextPage = () => {
   const [storedNumber, setStoredNumber] = useState(0);
   const [sign, setSign] = useState("");
   const [negative, setNegative] = useState(false);
+  const [decimal, setDecimal] = useState(false);
 
   return (
     <div className={styles.container}>
@@ -31,24 +32,24 @@ const Home: NextPage = () => {
           <ViewScreen number={viewedNumber}/>
 
           <div className={styles.buttonscontainer}>
-            <ClearButton handleClick={() => handleClearClick(viewedNumber, sign, setViewedNumber, setStoredNumber, setSign, setNegative)} />
+            <ClearButton handleClick={() => handleClearClick(viewedNumber, sign, setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal)} />
             <NegativeButton handleClick={() => handleNegativeClick(negative, viewedNumber, setNegative, setViewedNumber)} />
-            <DivideButton handleClick={() => handleSignClick(viewedNumber, storedNumber, sign, "/", setViewedNumber, setStoredNumber, setSign, setNegative)} />
-            <NumberButton number={7} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 7, negative, setNegative))} />
-            <NumberButton number={8} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 8, negative, setNegative))} />
-            <NumberButton number={9} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 9, negative, setNegative))} />
-            <MultiplyButton handleClick={() => handleSignClick(viewedNumber, storedNumber, sign, "*", setViewedNumber, setStoredNumber, setSign, setNegative)} />
-            <NumberButton number={4} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 4, negative, setNegative))} />
-            <NumberButton number={5} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 5, negative, setNegative))} />
-            <NumberButton number={6} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 6, negative, setNegative))} />
-            <SubtractButton handleClick={() => handleSignClick(viewedNumber, storedNumber, sign, "-", setViewedNumber, setStoredNumber, setSign, setNegative)} />
-            <NumberButton number={1} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 1, negative, setNegative))} />
-            <NumberButton number={2} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 2, negative, setNegative))} />
-            <NumberButton number={3} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 3, negative, setNegative))} />
-            <AddButton handleClick={() => handleSignClick(viewedNumber, storedNumber, sign, "+", setViewedNumber, setStoredNumber, setSign, setNegative)} />
-            <ZeroButton handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 0, negative, setNegative))} />
-            <DecimalButton />
-            <EqualsButton handleClick={() => handleEqualsClick(storedNumber, viewedNumber, sign, setSign, setStoredNumber, setViewedNumber, setNegative)} />
+            <DivideButton handleClick={() => handleSignClick(viewedNumber, storedNumber, sign, "/", setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal)} />
+            <NumberButton number={7} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 7, negative, decimal))} />
+            <NumberButton number={8} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 8, negative, decimal))} />
+            <NumberButton number={9} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 9, negative, decimal))} />
+            <MultiplyButton handleClick={() => handleSignClick(viewedNumber, storedNumber, sign, "*", setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal)} />
+            <NumberButton number={4} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 4, negative, decimal))} />
+            <NumberButton number={5} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 5, negative, decimal))} />
+            <NumberButton number={6} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 6, negative, decimal))} />
+            <SubtractButton handleClick={() => handleSignClick(viewedNumber, storedNumber, sign, "-", setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal)} />
+            <NumberButton number={1} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 1, negative, decimal))} />
+            <NumberButton number={2} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 2, negative, decimal))} />
+            <NumberButton number={3} handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 3, negative, decimal))} />
+            <AddButton handleClick={() => handleSignClick(viewedNumber, storedNumber, sign, "+", setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal)} />
+            <ZeroButton handleClick={() => setViewedNumber(handleNumberClick(viewedNumber, 0, negative, decimal))} />
+            <DecimalButton handleClick={() => handleDecimalClick(viewedNumber, decimal, setDecimal)} />
+            <EqualsButton handleClick={() => handleEqualsClick(storedNumber, viewedNumber, sign, setSign, setStoredNumber, setViewedNumber, setNegative, setDecimal)} />
           </div>
         </div>
       </main>
