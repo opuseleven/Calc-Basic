534877453f9872bd8f457f574a6618f6cca46c77
Cody Clark <opuseleven@protonmail.com>
2022-02-19 13:31:29 -0600

    Added a layer of safety to handle equalsClick without assigned sign.

--- services/EqualsClick.ts
+++ services/EqualsClick.ts
@@ -4,7 +4,13 @@ function handleEqualsClick(storedNumber: number, viewedNumber: number, sign: str
   setStoredNumber: React.Dispatch<React.SetStateAction<number>>, setViewedNumber: React.Dispatch<React.SetStateAction<number>>,
   setNegative: React.Dispatch<React.SetStateAction<boolean>>, setDecimal: React.Dispatch<React.SetStateAction<boolean>>) {
 
-  const total: number = evaluate(storedNumber, sign, viewedNumber);
+  let total: number;
+
+  if (sign.length < 1) {
+    total = viewedNumber;
+  } else {
+    total = evaluate(storedNumber, sign, viewedNumber);
+  }
 
   setStoredNumber(0);
   setSign("");
