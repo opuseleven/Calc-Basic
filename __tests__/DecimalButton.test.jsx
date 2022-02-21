93f00299d8ae4ba64496313be3c8167363422f30
Cody Clark <opuseleven@protonmail.com>
2022-02-21 00:03:17 -0600

    Added tests for functionality of Decimal Button.

--- __tests__/DecimalButton.test.jsx
+++ __tests__/DecimalButton.test.jsx
@@ -1,10 +1,34 @@
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { DecimalButton } from '../components';
+import { handleDecimalClick } from '../services';
 
 describe('Decimal Button Component', () => {
+
+  let viewedNumber = 0;
+  let decimal = false;
+  function setDecimal(bool) {
+    decimal = bool;
+  }
+
   it('Renders', () => {
     render(<DecimalButton />);
     expect(screen.getByRole('button')).toHaveTextContent('.');
+  })
+
+  it('Set Decimal function', () => {
+    handleDecimalClick(viewedNumber, decimal, setDecimal);
+    expect(decimal).toBe(true);
+    handleDecimalClick(viewedNumber, decimal, setDecimal);
+    expect(decimal).toBe(false);
+  })
+  it('Edge Case', () => {
+    decimal = true;
+    viewedNumber = 2.5;
+    handleDecimalClick(viewedNumber, decimal, setDecimal);
+    expect(decimal).toBe(true);
+    viewedNumber = 2;
+    handleDecimalClick(viewedNumber, decimal, setDecimal);
+    expect(decimal).toBe(false);
   })
 })
