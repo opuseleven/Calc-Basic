89351815fcad02d696c2bda0a811a717a6662a3b
Cody Clark <opuseleven@protonmail.com>
2022-02-21 12:29:57 -0600

    Added tests for handleNegativeClick function.

--- __tests__/NegativeButton.test.jsx
+++ __tests__/NegativeButton.test.jsx
@@ -1,10 +1,34 @@
-import { render, screen } from '@testing-library/react';
+import { render, fireEvent, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { NegativeButton } from '../components';
+import { handleNegativeClick } from '../services';
 
 describe('Negative Button Component', () => {
+
+  let viewedNumber = 0;
+  function setViewedNumber(n) {
+    viewedNumber = n;
+  }
+  let negative = false;
+  function setNegative(bool) {
+    negative = bool;
+  }
+
   it('Renders', () => {
     render(<NegativeButton />);
     expect(screen.getByRole('button')).toHaveTextContent('+/-');
+  })
+
+  it('Sets Negative bool', () => {
+    render(<NegativeButton handleClick={handleNegativeClick(negative, viewedNumber, setNegative, setViewedNumber)} />);
+    fireEvent.click(screen.getByText("+/-"));
+    expect(negative).toBe(true);
+  })
+
+  it('Sets value of viewed Number to negative', () => {
+    viewedNumber = 3;
+    negative = false;
+    handleNegativeClick(negative, viewedNumber, setNegative, setViewedNumber);
+    expect(viewedNumber).toBe(-3);
   })
 })
