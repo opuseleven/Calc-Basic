dd8ef5166c3e92b076cd41a6ff912b758da13e49
Cody Clark <opuseleven@protonmail.com>
2022-10-30 11:04:50 -0500

    Re-wrote ClearButton component to move functionality to component

--- components/ClearButton.tsx
+++ components/ClearButton.tsx
@@ -1,14 +1,32 @@
 import styles from '../styles/Components.module.css';
+import { handleClearClick } from '../services';
+import { FC, Dispatch, SetStateAction } from 'react';
 
 interface ClearButtonProps {
-  handleClick: VoidFunction
+  viewedNumber: number
+  sign: string
+  setViewedNumber: Dispatch<SetStateAction<number>>,
+  setStoredNumber: Dispatch<SetStateAction<number>>,
+  setSign: Dispatch<SetStateAction<string>>,
+  setNegative: Dispatch<SetStateAction<boolean>>,
+  setDecimal: Dispatch<SetStateAction<boolean>>
 }
 
-const ClearButton: React.FC<ClearButtonProps> = ({ handleClick }) => {
+const ClearButton: FC<ClearButtonProps> = (
+  {
+    viewedNumber, sign, setViewedNumber, setStoredNumber,
+    setSign, setNegative, setDecimal
+  }) => {
+
+  function handleClick() {
+    handleClearClick(viewedNumber, sign, setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal);
+  }
 
   return (
     <div className={styles.clearbuttoncontainer}>
-      <button className={styles.clearbutton} onClick={handleClick}>Clear</button>
+      <button className={styles.clearbutton} onClick={() => handleClick()}>
+        Clear
+      </button>
     </div>
   )
 }
