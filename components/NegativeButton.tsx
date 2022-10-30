447f7bd32705bd2666e91a5018cf13745d51ba5a
Cody Clark <opuseleven@protonmail.com>
2022-10-30 11:42:06 -0500

    Moved Negative button functionality to component

--- components/NegativeButton.tsx
+++ components/NegativeButton.tsx
@@ -1,14 +1,26 @@
 import styles from '../styles/Components.module.css';
+import { FC, Dispatch, SetStateAction } from 'react';
+import { handleNegativeClick } from '../services';
 
 interface NegativeButtonProps {
-  handleClick: VoidFunction
+  negative: boolean,
+  viewedNumber: number,
+  setNegative: Dispatch<SetStateAction<boolean>>,
+  setViewedNumber: Dispatch<SetStateAction<number>>
 }
 
-const NegativeButton: React.FC<NegativeButtonProps> = ({ handleClick }) => {
+const NegativeButton: FC<NegativeButtonProps> =
+  ({ negative, viewedNumber, setNegative, setViewedNumber }) => {
+
+  function handleClick() {
+    handleNegativeClick(negative, viewedNumber, setNegative, setViewedNumber);
+  }
 
   return (
     <div className={styles.negativebutton}>
-      <button className={styles.bluebutton} onClick={handleClick}>+/-</button>
+      <button className={styles.bluebutton} onClick={() => handleClick()}>
+        +/-
+      </button>
     </div>
   )
 }
