8eca5a213a59a2a8e16d6de73c31f1e57f7fcfdb
Cody Clark <opuseleven@protonmail.com>
2022-10-30 12:08:11 -0500

    Fixed tests for DivideButton component

--- __tests__/DivideButton.test.jsx
+++ __tests__/DivideButton.test.jsx
@@ -1,10 +1,42 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { DivideButton } from '../components';
 
 describe('Divide Button Component', () => {
-  it('Renders', () => {
-    render(<DivideButton />);
+
+  let viewedNumber = 0;
+  function setViewedNumber(num) {
+    viewedNumber = num;
+  }
+  let storedNumber = 0;
+  function setStoredNumber(num) {
+    storedNumber = num;
+  }
+  let sign = "";
+  function setSign(s) {
+    sign = s;
+  }
+  function setNegative(bool) {
+    // do nothing
+  }
+  function setDecimal(bool) {
+    // do nothing
+  }
+
+  it('Component renders', () => {
+    render(<DivideButton viewedNumber={viewedNumber} storedNumber={storedNumber}
+      sign={sign} setSign={setSign} setViewedNumber={setViewedNumber}
+      setStoredNumber={setStoredNumber} setNegative={setNegative}
+      setDecimal={setDecimal} />);
     expect(screen.getByRole('button')).toHaveTextContent('/');
+  })
+  it('Component functions', () => {
+    render(<DivideButton viewedNumber={viewedNumber} storedNumber={storedNumber}
+      sign={sign} setSign={setSign} setViewedNumber={setViewedNumber}
+      setStoredNumber={setStoredNumber} setNegative={setNegative}
+      setDecimal={setDecimal} />);
+    expect(screen.getByRole('button')).toHaveTextContent('/');
+    fireEvent.click(screen.getByText('/'));
+    expect(sign).toBe('/');
   })
 })
