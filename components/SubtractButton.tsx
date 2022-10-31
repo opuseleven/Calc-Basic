36a33d645435cce59ac06fc58768e6b1ac97cc5b
Cody Clark <opuseleven@protonmail.com>
2022-10-31 12:41:21 -0500

    Moved functionality to SubtractButton component.

--- components/SubtractButton.tsx
+++ components/SubtractButton.tsx
@@ -1,17 +1,35 @@
 import styles from '../styles/Components.module.css';
+import { handleSignClick } from '../services';
+import { FC, Dispatch, SetStateAction } from 'react';
 
 interface SubtractButtonProps {
-  handleClick: VoidFunction,
-  sign: string
+  viewedNumber: number,
+  storedNumber: number,
+  sign: string,
+  setViewedNumber: Dispatch<SetStateAction<number>>,
+  setStoredNumber: Dispatch<SetStateAction<number>>,
+  setSign: Dispatch<SetStateAction<string>>,
+  setNegative: Dispatch<SetStateAction<boolean>>,
+  setDecimal: Dispatch<SetStateAction<boolean>>
 }
 
-const SubtractButton: React.FC<SubtractButtonProps> = ({ handleClick, sign }) => {
+const SubtractButton: FC<SubtractButtonProps> =
+  ({ viewedNumber, storedNumber, sign, setViewedNumber, setStoredNumber, setSign,
+    setNegative, setDecimal }) => {
+
+function handleClick() {
+  handleSignClick(viewedNumber, storedNumber, sign, "-", setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal);
+}
 
   return (
     <div className={styles.subtractbutton}>
+
       <button className={sign === '-' ? styles.selectedsignbutton : styles.signbutton}
-        onClick={handleClick}
-      >-</button>
+        onClick={() => handleClick()}
+      >
+        -
+      </button>
+
     </div>
   )
 }
