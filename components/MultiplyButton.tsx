7f99f70e2e7d412ec7550b13f79a9a52f46e3d96
Cody Clark <opuseleven@protonmail.com>
2022-10-31 11:45:28 -0500

    Moved functionality to component.

--- components/MultiplyButton.tsx
+++ components/MultiplyButton.tsx
@@ -1,17 +1,35 @@
 import styles from '../styles/Components.module.css';
+import { handleSignClick } from '../services';
+import { FC, Dispatch, SetStateAction } from 'react';
 
 interface MultiplyButtonProps {
-  handleClick: VoidFunction,
-  sign: string
+  viewedNumber: number,
+  storedNumber: number,
+  sign: string,
+  setViewedNumber: Dispatch<SetStateAction<number>>,
+  setStoredNumber: Dispatch<SetStateAction<number>>,
+  setSign: Dispatch<SetStateAction<string>>,
+  setNegative: Dispatch<SetStateAction<boolean>>,
+  setDecimal: Dispatch<SetStateAction<boolean>>
 }
 
-const MultiplyButton: React.FC<MultiplyButtonProps> = ({ handleClick, sign }) => {
+const MultiplyButton: FC<MultiplyButtonProps> =
+  ({ viewedNumber, storedNumber, sign, setViewedNumber, setStoredNumber, setSign,
+    setNegative, setDecimal }) => {
+
+  function handleClick() {
+    handleSignClick(viewedNumber, storedNumber, sign, "*", setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal);
+  }
 
   return (
     <div className={styles.multiplybutton}>
+
       <button className={sign === '*' ? styles.selectedsignbutton : styles.signbutton}
-        onClick={handleClick}
-      >x</button>
+        onClick={() => handleClick()}
+      >
+        x
+      </button>
+
     </div>
   )
 }
