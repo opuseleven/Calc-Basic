8efb6ca6661b830d79aff346c7225dffd2d9512a
Cody Clark <opuseleven@protonmail.com>
2022-10-31 15:01:10 -0500

    Renamed NegativeButton service to handleNegativeClick

--- services/index.js
+++ services/index.js
@@ -3,5 +3,5 @@ export * from './handleClearClick';
 export * from './SignClick';
 export * from './Evaluate';
 export * from './handleEqualsClick';
-export * from './NegativeButton';
+export * from './handleNegativeClick';
 export * from './handleDecimalClick';
