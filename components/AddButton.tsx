5d39bc1ac2a180072aa7790c06c20cf590bb92f1
Cody Clark <opuseleven@protonmail.com>
2022-10-31 13:17:31 -0500

    Moved functionality to AddButton component.

--- components/AddButton.tsx
+++ components/AddButton.tsx
@@ -1,17 +1,35 @@
 import styles from '../styles/Components.module.css';
+import { FC, Dispatch, SetStateAction } from 'react';
+import { handleSignClick } from '../services';
 
 interface AddButtonProps {
-  handleClick: VoidFunction,
-  sign: string
+  viewedNumber: number,
+  storedNumber: number,
+  sign: string,
+  setViewedNumber: Dispatch<SetStateAction<number>>,
+  setStoredNumber: Dispatch<SetStateAction<number>>,
+  setSign: Dispatch<SetStateAction<string>>,
+  setNegative: Dispatch<SetStateAction<boolean>>,
+  setDecimal: Dispatch<SetStateAction<boolean>>
 }
 
-const AddButton: React.FC<AddButtonProps> = ({ handleClick, sign }) => {
+const AddButton: FC<AddButtonProps> =
+  ({ viewedNumber, storedNumber, sign, setViewedNumber, setStoredNumber, setSign,
+    setNegative, setDecimal }) => {
+
+  function handleClick() {
+    handleSignClick(viewedNumber, storedNumber, sign, "+", setViewedNumber, setStoredNumber, setSign, setNegative, setDecimal)
+  }
 
   return (
     <div className={styles.addbutton}>
+
       <button className={sign === '+' ? styles.selectedsignbutton : styles.signbutton}
-        onClick={handleClick}
-      >+</button>
+        onClick={() => handleClick()}
+      >
+        +
+      </button>
+
     </div>
   )
 }
