9d7fa083379e95e98122819e5063f8087f4a81c2
Cody Clark <opuseleven@protonmail.com>
2022-10-31 11:11:43 -0500

    Moved NumberButton functionality to component.

--- components/NumberButton.tsx
+++ components/NumberButton.tsx
@@ -1,17 +1,31 @@
 import styles from '../styles/Components.module.css';
+import { handleNumberClick } from '../services';
+import { FC, Dispatch, SetStateAction } from 'react';
 
 interface NumberButtonProps {
   number: number,
-  handleClick: VoidFunction
+  viewedNumber: number,
+  setViewedNumber: Dispatch<SetStateAction<number>>,
+  negative: boolean,
+  decimal: boolean
 }
 
-const NumberButton: React.FC<NumberButtonProps> = ({ number, handleClick }) => {
+const NumberButton: FC<NumberButtonProps> =
+  ({ number, viewedNumber, setViewedNumber, negative, decimal }) => {
 
   const numberstring = String(number);
 
+  function handleClick() {
+    setViewedNumber(handleNumberClick(viewedNumber, number, negative, decimal))
+  }
+
   return (
     <div className={styles[`button${numberstring}`]}>
-      <button className={styles.button} onClick={handleClick}>{number}</button>
+
+      <button className={styles.button} onClick={handleClick}>
+        {number}
+      </button>
+      
     </div>
   )
 }
