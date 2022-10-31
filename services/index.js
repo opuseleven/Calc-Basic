43b6ed2100ac4c8bfa38fafb0c77a519aa438304
Cody Clark <opuseleven@protonmail.com>
2022-10-31 14:58:37 -0500

    Renamed EqualsClick service to handleEqualsClick

--- services/index.js
+++ services/index.js
@@ -2,6 +2,6 @@ export * from './NumberServices';
 export * from './handleClearClick';
 export * from './SignClick';
 export * from './Evaluate';
-export * from './EqualsClick';
+export * from './handleEqualsClick';
 export * from './NegativeButton';
 export * from './handleDecimalClick';
