bb16c363cd8c80a8593defa5bdea5d3c5e985952
Cody Clark <opuseleven@protonmail.com>
2022-10-31 14:15:02 -0500

    Moved functionality to EqualsButton component

--- components/EqualsButton.tsx
+++ components/EqualsButton.tsx
@@ -1,14 +1,33 @@
 import styles from '../styles/Components.module.css';
+import { FC, Dispatch, SetStateAction } from 'react';
+import { handleEqualsClick } from '../services';
 
 interface EqualsButtonProps {
-  handleClick: VoidFunction
+  storedNumber: number,
+  viewedNumber: number,
+  sign: string,
+  setSign: Dispatch<SetStateAction<string>>,
+  setStoredNumber: Dispatch<SetStateAction<number>>,
+  setViewedNumber: Dispatch<SetStateAction<number>>,
+  setNegative: Dispatch<SetStateAction<boolean>>,
+  setDecimal: Dispatch<SetStateAction<boolean>>
 }
 
-const EqualsButton: React.FC<EqualsButtonProps> = ({ handleClick }) => {
+const EqualsButton: FC<EqualsButtonProps> =
+  ({ storedNumber, viewedNumber, sign, setSign, setStoredNumber, setViewedNumber,
+    setNegative, setDecimal }) => {
+
+  function handleClick() {
+    handleEqualsClick(storedNumber, viewedNumber, sign, setSign, setStoredNumber, setViewedNumber, setNegative, setDecimal);
+  }
 
   return (
     <div className={styles.equalsbuttoncontainer}>
-      <button className={styles.equalsbutton} onClick={handleClick}>=</button>
+
+      <button className={styles.equalsbutton} onClick={() => handleClick()}>
+        =
+      </button>
+
     </div>
   )
 }
