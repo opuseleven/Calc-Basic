f472b2e2143aff4cdda88b5b68a165ba24100167
Cody Clark <opuseleven@protonmail.com>
2022-10-31 13:37:53 -0500

    Added tests for functionality of ZeroButton component

--- __tests__/ZeroButton.test.jsx
+++ __tests__/ZeroButton.test.jsx
@@ -1,10 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { ZeroButton } from '../components';
 
 describe('Zero Button Component', () => {
-  it('Renders', () => {
-    render(<ZeroButton />);
+
+  let viewedNumber = 0;
+  function setViewedNumber(num) {
+    viewedNumber = num;
+  }
+  let negative = false;
+  let decimal = false;
+
+  it('Component renders', () => {
+    render(<ZeroButton viewedNumber={viewedNumber} setViewedNumber={setViewedNumber}
+      negative={negative} decimal={decimal} />);
     expect(screen.getByRole('button')).toHaveTextContent('0');
+  })
+
+  it('Component functions to set viewedNumber', () => {
+    render(<ZeroButton viewedNumber={viewedNumber} setViewedNumber={setViewedNumber}
+      negative={negative} decimal={decimal} />);
+    fireEvent.click(screen.getByText('0'));
+    expect(viewedNumber).toBe(0);
+  })
+
+  it('Sets viewed number if decimal=true', () => {
+    decimal = true;
+    setViewedNumber(1);
+    render(<ZeroButton viewedNumber={viewedNumber} setViewedNumber={setViewedNumber}
+      negative={negative} decimal={decimal} />);
+    fireEvent.click(screen.getByText('0'));
+    expect(viewedNumber).toBe(1.0)
+  })
+
+  it('Sets viewedNumber if negative=true', () => {
+    decimal = false;
+    negative = true;
+    setViewedNumber(0);
+    render(<ZeroButton viewedNumber={viewedNumber} setViewedNumber={setViewedNumber}
+      negative={negative} decimal={decimal} />);
+    fireEvent.click(screen.getByText('0'));
+    expect(viewedNumber).toBe(-0);
+  })
+
+  it('Sets viewedNumber if both negative&decimal=true', () => {
+    setViewedNumber(0);
+    negative = true;
+    decimal = true;
+    render(<ZeroButton viewedNumber={viewedNumber} setViewedNumber={setViewedNumber}
+      negative={negative} decimal={decimal} />);
+    fireEvent.click(screen.getByText('0'));
+    expect(viewedNumber).toBe(-0.0);
   })
 })
