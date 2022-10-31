7ff2ce391f870f28a48b9810177b357610d6de12
Cody Clark <opuseleven@protonmail.com>
2022-10-31 13:37:22 -0500

    Moved functionality to ZeroButton component

--- components/ZeroButton.tsx
+++ components/ZeroButton.tsx
@@ -1,14 +1,28 @@
 import styles from '../styles/Components.module.css';
+import { handleNumberClick } from '../services';
+import { FC, Dispatch, SetStateAction } from 'react';
 
 interface ZeroButtonProps {
-  handleClick: VoidFunction
+  viewedNumber: number,
+  negative: boolean,
+  decimal: boolean,
+  setViewedNumber: Dispatch<SetStateAction<number>>
 }
 
-const ZeroButton: React.FC<ZeroButtonProps> = ({ handleClick }) => {
+const ZeroButton: FC<ZeroButtonProps> =
+  ({ viewedNumber, negative, decimal, setViewedNumber }) => {
+
+  function handleClick() {
+    setViewedNumber(handleNumberClick(viewedNumber, 0, negative, decimal));
+  }
 
   return (
     <div className={styles.zerobutton}>
-      <button className={styles.button} onClick={handleClick}>0</button>
+
+      <button className={styles.button} onClick={() => handleClick()}>
+        0
+      </button>
+
     </div>
   )
 }
