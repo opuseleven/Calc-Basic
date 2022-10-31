9382c596a9c1cf08d6c9ed4e033d0a56056b2e15
Cody Clark <opuseleven@protonmail.com>
2022-10-31 11:45:46 -0500

    Added additional tests for functionality of MultiplyButton component

--- __tests__/MultiplyButton.test.jsx
+++ __tests__/MultiplyButton.test.jsx
@@ -1,10 +1,55 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { MultiplyButton } from '../components';
 
 describe('Multiply Button Component', () => {
-  it('Renders', () => {
+
+  let viewedNumber = 8;
+  function setViewedNumber(num){
+    viewedNumber = num;
+  }
+  let storedNumber = 0;
+  function setStoredNumber(num) {
+    storedNumber = num;
+  }
+  let sign = "";
+  function setSign(s) {
+    sign = s;
+  }
+  let negative = false;
+  function setNegative(bool) {
+    negative = bool;
+  }
+  let decimal = false;
+  function setDecimal(bool) {
+    decimal = bool;
+  }
+
+  it('Component renders', () => {
     render(<MultiplyButton />);
     expect(screen.getByRole('button')).toHaveTextContent('x');
+  })
+
+  it('Component functions to set sign var', () => {
+    render(<MultiplyButton viewedNumber={viewedNumber} setViewedNumber={setViewedNumber}
+      storedNumber={storedNumber} setStoredNumber={setStoredNumber} sign={sign}
+      setSign={setSign} setNegative={setNegative} setDecimal={setDecimal} />);
+    fireEvent.click(screen.getByText('x'));
+    expect(storedNumber).toBe(8);
+    expect(sign).toBe('*');
+    expect(viewedNumber).toBe(0);
+  })
+  
+  it('Component functions to store viewed number, and set viewedNumber to 0', () => {
+    setSign('');
+    setViewedNumber(7);
+    setStoredNumber(0);
+    render(<MultiplyButton viewedNumber={viewedNumber} setViewedNumber={setViewedNumber}
+      storedNumber={storedNumber} setStoredNumber={setStoredNumber} sign={sign}
+      setSign={setSign} setNegative={setNegative} setDecimal={setDecimal} />);
+    fireEvent.click(screen.getByText('x'));
+    expect(storedNumber).toBe(7);
+    expect(sign).toBe('*');
+    expect(viewedNumber).toBe(0);
   })
 })
